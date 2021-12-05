2675ad20876fe3719e11ecb491629f509f7881bd
Stanislav Shurupin <stanislav.shurupin@gmail.com>
2021-12-05 17:50:33 +0300

    Added YoutubeExplode

--- YoutubeTimecodes/Services/YoutubeTimecodesService.cs
+++ YoutubeTimecodes/Services/YoutubeTimecodesService.cs
@@ -3,13 +3,17 @@
     using System;
     using System.Collections.Generic;
     using System.Threading.Tasks;
+    using YoutubeExplode;
+    using YoutubeExplode.Videos.ClosedCaptions;
 
     public class YoutubeTimecodesService : IYoutubeTimecodesService
     {
+        private readonly YoutubeClient _youtubeClient;
         private readonly ITextSummarizationService _textSummarizationService;
 
         public YoutubeTimecodesService(ITextSummarizationService textSummarizationService)
         {
+            this._youtubeClient = new YoutubeClient();
             this._textSummarizationService = textSummarizationService ?? throw new ArgumentNullException(paramName: nameof(textSummarizationService));
         }
 
@@ -17,7 +21,11 @@
         {
             try
             {
-                List<string> result = await this._textSummarizationService.GetSummariesAsync("");
+                ClosedCaptionManifest closedCaptionManifest = await this._youtubeClient.Videos.ClosedCaptions.GetManifestAsync(videoId: youtubeVideoUrl);
+                ClosedCaptionTrackInfo closedCaptionTrackInfo = closedCaptionManifest.GetByLanguage(language: "en");
+                ClosedCaptionTrack closedCaptionTrack = await this._youtubeClient.Videos.ClosedCaptions.GetAsync(trackInfo: closedCaptionTrackInfo);
+                string text = string.Join(". ", closedCaptionTrack.Captions);
+                List<string> result = await this._textSummarizationService.GetSummariesAsync(text);
 
                 return result;
             }
